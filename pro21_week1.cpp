47eaf13366a6896c06765beb70a75eade39366a9
aya khaled <124468391+Ayakhaled0197@users.noreply.github.com>
2023-07-12 16:09:12 +0300

    Add files via upload

---
+++ pro21_week1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<iomanip>
+#include<stdio.h>
+#include<cmath>
+
+using namespace std;
+
+int main(){
+    long long l1 , l2 , r1 , r2 , a , b;
+    cin>>l1>>l2>>r1>>r2;
+    if(a>=l1 && a<=r1 && a>=l2 )
+    if((a>=l1 && a<=r1)&&(b>=l1 && b<=r1)) cout<<min(a , b);
+}
